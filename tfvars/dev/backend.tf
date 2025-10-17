13a5c8aee4d83d3995a110ebcb70940a775be1fc
naginenihari <naginenihari@gmail.com>
2025-10-17 22:33:10 +0530

    multi environment creation

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+bucket = "remote-state-aws-terraform-dev"
+key    = "dev-remote-state-file"
+region = "us-east-1"
+use_lockfile = true
+encrypt = true
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket = "remote-state-aws-terraform-prod"
+key    = "prod-remote-state-file"
+region = "us-east-1"
+use_lockfile = true
+encrypt = true
